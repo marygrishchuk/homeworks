90c1852464686f72c27d5498febf567fd944345b
Maria Grishchuk <mary.grishchuk1@gmail.com>
2020-10-12 18:08:23 +0300

    Updates

---
+++ precache-manifest.5b7058f399a442fc37ea368f784405f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7d04da6a3ad820686b020cf2b85d63",
+    "url": "/homeworks/index.html"
+  },
+  {
+    "revision": "8b44de06d9d2d180ace0",
+    "url": "/homeworks/static/css/main.d129303f.chunk.css"
+  },
+  {
+    "revision": "540d18a27b0bb326b955",
+    "url": "/homeworks/static/js/2.2eac4424.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homeworks/static/js/2.2eac4424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b44de06d9d2d180ace0",
+    "url": "/homeworks/static/js/main.afd10c27.chunk.js"
+  },
+  {
+    "revision": "d03642954bab67b9ebe2",
+    "url": "/homeworks/static/js/runtime-main.ca69ec97.js"
+  },
+  {
+    "revision": "08ac7b57f150f3b37730e5bff6af777b",
+    "url": "/homeworks/static/media/imageArtem.08ac7b57.jpg"
+  }
+]);
